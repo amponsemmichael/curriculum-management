0771a0544e93ed981a7463fa2d914b58f406e829
amponsemmichael <amponsem_michael@yahoo.com>
2024-11-22 09:11:02 +0000

    refactor: simplify CurriculumEntity class structure
    Renamed CurriculumEntity to Curriculum.
    
    Removed thumbnailPath, learningObjectives, and modules fields for simplicity.
    
    Retained essential fields title and description.

--- src/main/java/com/amponsem/curriculum_management_service/entity/Curriculum.java
+++ src/main/java/com/amponsem/curriculum_management_service/entity/Curriculum.java
@@ -6,30 +6,11 @@ import lombok.EqualsAndHashCode;
 
 import java.util.List;
 
-@EqualsAndHashCode(callSuper = true)
-@Data
 @Entity
-@Table(name = "curricula")
-public class CurriculumEntity extends BaseEntity {
-    @Column(nullable = false, length = 255)
+public class Curriculum extends BaseEntity {
     private String title;
-
-    @Column(length = 1000)
     private String description;
 
-    @Column(length = 500)
-    private String thumbnailPath;
-
-    @ElementCollection
-    @CollectionTable(name = "curriculum_learning_objectives", joinColumns = @JoinColumn(name = "curriculum_id"))
-    @Column(name = "objective")
-    private List<String> learningObjectives;
-
 //    @ManyToOne
-//    @JoinColumn(name = "specialization_id")
 //    private Specialization specialization;
-
-    @OneToMany(mappedBy = "curriculum", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<CurriculumModule> modules;
 }
-
