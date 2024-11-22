eb42706769e2cc74b2b468618be388673bb1cc4c
amponsemmichael <amponsem_michael@yahoo.com>
2024-11-22 09:14:10 +0000

    feat: add CurriculumRepository interface
    Introduced CurriculumRepository to manage Curriculum entities.
    
    Added method findBySpecializationId to retrieve curricula by specialization ID.

--- src/main/java/com/amponsem/curriculum_management_service/repository/CurriculumRepository.java
+++ src/main/java/com/amponsem/curriculum_management_service/repository/CurriculumRepository.java
@@ -1,4 +1,10 @@
 package com.amponsem.curriculum_management_service.repository;
 
-public interface CurriculumRepository {
+import com.amponsem.curriculum_management_service.entity.Curriculum;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CurriculumRepository extends JpaRepository<Curriculum, String> {
+    List<Curriculum> findBySpecializationId(String specializationId);
 }
