38556a961480d518dbb406d25021cc767b91af59
amponsemmichael <amponsem_michael@yahoo.com>
2024-11-22 10:57:18 +0000

    feat: define createCurriculum method in CurriculumService interface
    Introduced CurriculumService interface with the createCurriculum method.
    
    Added the createCurriculum method to handle the creation of new curricula using CurriculumDto.

--- src/main/java/com/amponsem/curriculum_management_service/service/CurriculumService.java
+++ src/main/java/com/amponsem/curriculum_management_service/service/CurriculumService.java
@@ -1,4 +1,9 @@
-package com.amponsem.curriculum_management_service.service.impl;
+package com.amponsem.curriculum_management_service.service;
+
+import com.amponsem.curriculum_management_service.dto.CurriculumDto;
+import com.amponsem.curriculum_management_service.entity.Curriculum;
+import jakarta.validation.Valid;
 
 public interface CurriculumService {
+    Curriculum createCurriculum(@Valid CurriculumDto curriculum);
 }
