e4a1a1f4905d695f85f7b63699f224d71829f8cf
amponsemmichael <amponsem_michael@yahoo.com>
2024-11-22 10:58:44 +0000

    fix: implement createCurriculum method in CurriculumServiceImpl
    Injected CurriculumRepository properly into CurriculumServiceImpl.
    
    Implemented createCurriculum method to save new curricula using CurriculumDto.

--- src/main/java/com/amponsem/curriculum_management_service/service/impl/CurriculumServiceImpl.java
+++ src/main/java/com/amponsem/curriculum_management_service/service/impl/CurriculumServiceImpl.java
@@ -4,21 +4,24 @@ import com.amponsem.curriculum_management_service.dto.CurriculumDto;
 import com.amponsem.curriculum_management_service.entity.Curriculum;
 import com.amponsem.curriculum_management_service.repository.CurriculumRepository;
 import com.amponsem.curriculum_management_service.service.CurriculumService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CurriculumServiceImpl implements CurriculumService {
 
+    private final CurriculumRepository curriculumRepository;
+
     @Autowired
-    public CurriculumServiceImpl(CurriculumRepository curriculumRepository) {
-
-
+    public CurriculumServiceImpl(final CurriculumRepository curriculumRepository, CurriculumRepository curriculumRepository1) {
+        this.curriculumRepository = curriculumRepository1;
     }
 
     @Override
-    public Curriculum createCurriculum(Curriculum curriculum) {
-        return null;
+    public Curriculum createCurriculum(final @Valid CurriculumDto curriculum) {
+      return curriculumRepository.save(curriculum);
     }
 }
+
 
